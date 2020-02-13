6b1fa4798078d2467117a7d6e2b2beaad7a1a196
David Tate <djtate@gmail.com>
2020-02-13 07:02:32 +0100

    Update generateProgramme.php

--- assets/scripts/classes/generateProgramme.php
+++ assets/scripts/classes/generateProgramme.php
@@ -21,7 +21,29 @@ $openaccess =1;
 
             $data = json_decode(file_get_contents('php://input'), true);
 
+            if ( ! function_exists( 'array_key_last' ) ) {
+                /**
+                 * Polyfill for array_key_last() function added in PHP 7.3.
+                 *
+                 * Get the last key of the given array without affecting
+                 * the internal array pointer.
+                 *
+                 * @param array $array An array
+                 *
+                 * @return mixed The last key of array if the array is not empty; NULL otherwise.
+                 */
+                function array_key_last( $array ) {
+                    $key = NULL;
             
+                    if ( is_array( $array ) ) {
+            
+                        end( $array );
+                        $key = key( $array );
+                    }
+            
+                    return $key;
+                }
+            }
 
             //$sessionid = $data['sessionid'];
 
