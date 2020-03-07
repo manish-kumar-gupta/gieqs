aba2084443f2cf2795be0f75ff89e266c0c96ee3
David Tate <djtate@gmail.com>
2020-03-07 14:41:51 +0100

    Update index.php

--- index.php
+++ index.php
@@ -333,7 +333,7 @@ echo '<div id="signup" style="display:none;">' . $signup . '</div>';
     <!-- Google maps -->
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCBuyKngB9VC3zgY_uEB-DKL9BKYMekbeY"></script>
     <!-- Purpose JS -->
-    <!-- <script src="assets/js/purpose.js"></script> -->
+    <script src="assets/js/purpose.js"></script>
     <!-- <script src="assets/js/generaljs.js"></script> -->
     <script src="assets/js/demo.js"></script>
 
