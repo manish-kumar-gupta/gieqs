19489d696fcae927fc952f2da5aeab8a0e597812
David Tate <djtate@gmail.com>
2020-04-13 10:39:59 +0200

    Update index.php

--- edm/index.php
+++ edm/index.php
@@ -452,19 +452,20 @@
 			
 		
 
-switch (true) {
-	case winLocation('endoscopy.wiki'):
+			switch (true) {
+			case winLocation('gieqs.com'):
 
-		var rootFolder = 'https://www.endoscopy.wiki/esd/';
-		break;
-	case winLocation('localhost'):
-		var rootFolder = 'http://localhost:90/dashboard/esd/';
-		break;
-	default: // set whatever you want
-		var rootFolder = 'https://www.endoscopy.wiki/esd/';
-		break;
-}
+				var rootFolder = 'https://www.gieqs.com/edm/';
+				break;
 
+			case winLocation('localhost'):
+				var rootFolder = 'http://localhost:90/dashboard/gieqs/edm/';
+				break;
+
+			default: // set whatever you want
+				var rootFolder = 'https://www.gieqs.com/edm/';
+				break;
+		}
 var siteRoot = rootFolder;
 
 /*function mobileMenuShow(){
