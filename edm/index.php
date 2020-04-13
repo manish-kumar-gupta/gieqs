9ae6c652f61e1dcadd4776cbe20d5ad9415b529d
David Tate <djtate@gmail.com>
2020-04-13 11:15:04 +0200

    screwed by the same prob
    V2

--- edm/index.php
+++ edm/index.php
@@ -5,7 +5,7 @@
 			
 		require ('includes/config.inc.php');
 		
-		require (BASE_URI.'/scripts/headerCreatorv2.php');
+		require (BASE_URI.'/scripts/headerCreatorV2.php');
 		
 		/*
 		$host = substr($_SERVER['HTTP_HOST'], 0, 5);
