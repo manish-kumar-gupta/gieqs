d79e4538fbfc0f6c63cccf3a49620fd31e97b969
David Tate <djtate@gmail.com>
2020-07-05 17:22:52 +0200

    Update endowiki-player.js

--- pages/learning/includes/endowiki-player.js
+++ pages/learning/includes/endowiki-player.js
@@ -635,7 +635,7 @@ $(document).ready(function () {
 
 		$('#videoDescription').html('' + val.description + '');
 
-		$('#videoAuthor').html('<a class="text-muted" target="_blank" href="http://localhost:90/dashboard/gieqs/pages/learning/pages/account/public-profile.php?id=' + val.authorid + '">' + val.author + ', ' + val.centreName + ', ' + val.centreCountry + '</a>');
+		$('#videoAuthor').html('<a class="text-muted" target="_blank" href="'+siteRoot+'/pages/learning/pages/account/public-profile.php?id=' + val.authorid + '">' + val.author + ', ' + val.centreName + ', ' + val.centreCountry + '</a>');
 
 	})
 
