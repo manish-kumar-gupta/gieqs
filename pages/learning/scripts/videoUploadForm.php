5066f74297346eb4d5e94b6115a85728ead0c1bf
David Tate <djtate@gmail.com>
2020-09-15 10:20:24 +0200

    Update videoUploadForm.php
    updates to prevent duplicate tagging

--- pages/learning/scripts/videoUploadForm.php
+++ pages/learning/scripts/videoUploadForm.php
@@ -21,6 +21,10 @@
 
       $navigator = new navigator;
 
+      $usersTagging = new usersTagging;
+
+      $video_moderation = new video_moderation;
+
       //$video = new video;
 
       ?>
@@ -192,6 +196,29 @@ background-color: rgb(238, 194, 120);
     
     if (isset($_GET["id"]) && is_numeric($_GET["id"])){
         $id = $_GET["id"];
+
+        if ($isSuperuser == 0){
+
+            //terminate if there is an open invite
+
+            $openInvite =  $video_moderation->videoHasOpenTaggerInvite($id, $debug);
+
+            if ($openInvite){
+
+                $currentLockedUser = $video_moderation->getTagLockedUser($id, $debug);
+
+                if ($currentLockedUser[0] != $userid){
+
+                    echo 'This video is locked for tagging to another user';
+                    exit();
+                }
+
+            }
+
+
+            //and the logged in user is not that user
+
+        }
     
     }else{
     
