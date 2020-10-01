3529184d71fb0cdb10dacfc488ffbc0312441cc3
David Tate <djtate@gmail.com>
2020-10-01 06:56:13 +0200

    Update generateProgrammeCurrent.php

--- assets/scripts/classes/generateProgrammeCurrent.php
+++ assets/scripts/classes/generateProgrammeCurrent.php
@@ -76,7 +76,7 @@
 
             $edit = FALSE;
 
-            $debug = TRUE;
+            $debug = FALSE;
             
             if ($debug){
                 echo '<br/><br/>Data Array contains </br></br>';
