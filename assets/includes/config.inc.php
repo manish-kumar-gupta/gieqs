5a0584f1a92bbbdbf2eb29d8af644d1c5452b2b5
David Tate <djtate@gmail.com>
2020-10-31 10:36:49 +0100

    Update config.inc.php

--- assets/includes/config.inc.php
+++ assets/includes/config.inc.php
@@ -136,7 +136,7 @@ $debug = TRUE;
  *  before this next conditional.
  */
 
-$debug = TRUE;
+$debug = FALSE;
 
 // Assume debugging is off. 
 if (!isset($debug)) {
@@ -193,16 +193,10 @@ if ($debug){
     
 
     $_SESSION['debug'] = true;
-    
-    ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-error_reporting(E_ALL);
+    error_reporting(E_ALL);
 
 }else{
 
     $_SESSION['debug'] = false;
-    ini_set('display_errors', '0');
-ini_set('display_startup_errors', '0');
-error_reporting(E_ALL);
     error_reporting(0);
 }
