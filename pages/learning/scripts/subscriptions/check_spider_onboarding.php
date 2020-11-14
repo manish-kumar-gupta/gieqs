7a868fe2c7afe73b20bf63aa49b7c03433bb6fb9
David Tate <djtate@gmail.com>
2020-11-14 06:27:30 +0100

    Update check_spider_onboarding.php

--- pages/learning/scripts/subscriptions/check_spider_onboarding.php
+++ pages/learning/scripts/subscriptions/check_spider_onboarding.php
@@ -3,6 +3,7 @@ $openaccess = 0;
 
 $requiredUserLevel = 1;
 
+error_reporting(E_ALL);
 require_once '../../../../assets/includes/config.inc.php';
 
 require BASE_URI . '/assets/scripts/interpretUserAccess.php';
