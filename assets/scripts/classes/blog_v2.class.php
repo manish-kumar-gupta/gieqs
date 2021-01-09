ebb9333495e72d0bf14276758971b28c73bf1fc8
David Tate <djtate@gmail.com>
2021-01-09 07:00:36 +0100

    modify

---
+++ assets/scripts/classes/blog_v2.class.php
@@ -0,0 +1,464 @@
+<?php
+/*
+ * Author: David Tate  - www.gieqs.com
+ *
+ * Create Date: 6-01-2021
+ *
+ * DJT 2019
+ *
+ * License: LGPL
+ *
+ */
+
+if (session_status() == PHP_SESSION_NONE) { //if there's no session_start yet...
+    session_start(); //do this
+}
+
+if ($_SESSION['debug'] == true){
+
+error_reporting(E_ALL);
+
+}else{
+
+error_reporting(0);
+	
+}
+
+Class blog_v2 {
+
+	private $id; //int(11)
+	private $video_id; //int(11)
+	private $user_id; //int(11)
+	private $title; //varchar(400)
+	private $description; //varchar(1000)
+	private $created; //timestamp
+	private $connection;
+
+	public function __construct(){
+        require_once 'DatabaseMyssqlPDOLearning.class.php';
+        $this->connection = new DataBaseMysqlPDOLearning();
+       
+}
+
+    /**
+     * New object to the class. Don�t forget to save this new object "as new" by using the function $class->Save_Active_Row_as_New();
+     *
+     */
+	public function New_blog_v2($video_id,$user_id,$title,$description,$created){
+		$this->video_id = $video_id;
+		$this->user_id = $user_id;
+		$this->title = $title;
+		$this->description = $description;
+		$this->created = $created;
+	}
+
+    /**
+     * Load one row into var_class. To use the vars use for exemple echo $class->getVar_name;
+     *
+     * @param key_table_type $key_row
+     *
+     */
+	public function Load_from_key($key_row){
+		$result = $this->connection->RunQuery("Select * from blog_v2 where id = \"$key_row\" ");
+		while($row = $result->fetch(PDO::FETCH_ASSOC)){
+			$this->id = $row["id"];
+			$this->video_id = $row["video_id"];
+			$this->user_id = $row["user_id"];
+			$this->title = $row["title"];
+			$this->description = $row["description"];
+			$this->created = $row["created"];
+		}
+	}
+    /**
+ * Load specified number of rows and output to JSON. To use the vars use for exemple echo $class->getVar_name;
+ *
+ * @param key_table_type $key_row
+ *
+ */
+	public function Load_records_limit_json($y, $x=0){
+$q = "Select * from `blog_v2` LIMIT " . $x . ", " . $y;
+		$result = $this->connection->RunQuery($q);
+							$rowReturn = array();
+						$x = 0;
+						$nRows = $result->rowCount();
+						if ($nRows > 0){
+
+					while($row = $result->fetch(PDO::FETCH_ASSOC)){
+			$rowReturn[$x]["id"] = $row["id"];
+			$rowReturn[$x]["video_id"] = $row["video_id"];
+			$rowReturn[$x]["user_id"] = $row["user_id"];
+			$rowReturn[$x]["title"] = $row["title"];
+			$rowReturn[$x]["description"] = $row["description"];
+			$rowReturn[$x]["created"] = $row["created"];
+		$x++;		}return json_encode($rowReturn);}
+
+			else{return FALSE;
+			}
+			
+	}
+    /**
+ * Load specified number of rows and output to JSON. To use the vars use for exemple echo $class->getVar_name;
+ *
+ * @param key_table_type $key_row
+ *
+ */
+	public function Return_row($key){
+$q = "Select * from `blog_v2` WHERE `id` = $key";
+		$result = $this->connection->RunQuery($q);
+							$rowReturn = array();
+						$x = 0;
+						$nRows = $result->rowCount();
+						if ($nRows > 0){
+
+					while($row = $result->fetch(PDO::FETCH_ASSOC)){
+			$rowReturn[$x]["id"] = $row["id"];
+			$rowReturn[$x]["video_id"] = $row["video_id"];
+			$rowReturn[$x]["user_id"] = $row["user_id"];
+			$rowReturn[$x]["title"] = $row["title"];
+			$rowReturn[$x]["description"] = $row["description"];
+			$rowReturn[$x]["created"] = $row["created"];
+		$x++;		}return json_encode($rowReturn);}
+
+			else{return FALSE;
+			}
+			
+	}
+    
+
+        public function Load_records_limit_json_datatables($y, $x = 0)
+            {
+            $q = "Select * from `blog_v2` LIMIT $x, $y";
+            $result = $this->connection->RunQuery($q);
+            $rowReturn = array();
+            $x = 0;
+            $nRows = $result->rowCount();
+            if ($nRows > 0) {
+
+                while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+
+                    $rowReturn['data'][] = array_map('utf8_encode', $row);
+                }
+            
+                return json_encode($rowReturn);
+
+            } else {
+                
+
+                //RETURN AN EMPTY ARRAY RATHER THAN AN ERROR
+                $rowReturn['data'] = [];
+                
+                return json_encode($rowReturn);
+            }
+
+        }
+
+    /**
+     * Checks if the specified record exists
+     *
+     * @param key_table_type $key_row
+     *
+     */
+	public function matchRecord($key_row){
+		$result = $this->connection->RunQuery("Select * from `blog_v2` where `id` = '$key_row' ");
+		$nRows = $result->rowCount();
+			if ($nRows == 1){
+				return TRUE;
+			}else{
+				return FALSE;
+			}
+	}
+
+    /**
+		* Return the number of rows
+		*/
+	public function numberOfRows(){
+		return $this->connection->TotalOfRows('blog_v2');
+	}
+
+    /**
+		* Insert statement using PDO
+		*/
+ public function prepareStatementPDO (){ 
+ //need to only update those which are set 
+ $ov = get_object_vars($this); 
+if ($ov['connection'] != ''){
+			unset($ov['connection']);
+		} 
+if ($ov['id'] != ''){
+			unset($ov['id']);
+		} 
+$ovMod = array(); 
+foreach ($ov as $key=>$value){
+
+			if ($value != ''){
+
+				$key = '`' . $key . '`';
+
+				$ovMod[$key] = $value;
+			}
+
+			}
+$ovMod2 = array(); 
+foreach ($ov as $key=>$value){
+
+			if ($value != ''){
+
+				$key = '' . $key . '';
+
+				$ovMod2[$key] = $value;
+			}
+
+		} 
+$ovMod3 = array(); 
+foreach ($ov as $key=>$value){
+
+			if ($value != ''){
+
+				$key = ':' . $key;
+
+				$ovMod3[$key] = $value;
+			}
+
+		} 
+foreach ($ovMod as $key => $value) {
+
+            $value = addslashes($value);
+			$value = "'$value'";
+			$updates[] = "$value";
+
+		} 
+$implodeArray = implode(', ', $updates); 
+//get number of terms in update
+					//need only the keys first
+
+					$keys = implode(", ", array_keys($ovMod));
+					$keys2 = implode(", ", array_keys($ovMod3));
+			
+//get number of keys
+
+				$numberOfTerms = count($ovMod);
+		
+//echo $numberOfTerms;
+
+		$termsToInsert = ''; 
+$x=0;
+
+		foreach ($ovMod as $key=>$value){
+
+			$termsToInsert .= ( $x !== ($numberOfTerms -1) ) ? "? ," : " ?";
+
+			$x++;
+
+		} 
+$q = "INSERT INTO `blog_v2` ($keys) VALUES ($keys2)";
+		
+ $stmt = $this->connection->prepare($q); 
+$stmt->execute($ovMod3); 
+return $this->connection->conn->lastInsertId(); 
+	}
+
+    /**
+		* Update statement using PDO
+		*/
+ public function prepareStatementPDOUpdate (){ 
+ //need to only update those which are set 
+ $ov = get_object_vars($this); 
+if ($ov['connection'] != ''){
+			unset($ov['connection']);
+		} 
+if ($ov['id'] != ''){
+			unset($ov['id']);
+		} 
+if ($ov['updated'] != ''){
+			unset($ov['updated']);
+		} 
+$ovMod = array(); 
+foreach ($ov as $key=>$value){
+
+			if ($value != ''){
+
+				$key = '`' . $key . '`';
+
+				$ovMod[$key] = $value;
+			}
+
+			}
+$ovMod2 = array(); 
+foreach ($ov as $key=>$value){
+
+			if ($value != ''){
+
+				$key = '' . $key . '';
+
+				$ovMod2[$key] = $value;
+			}
+
+		} 
+$ovMod3 = array(); 
+foreach ($ov as $key=>$value){
+
+			if ($value != ''){
+
+				$key = ':' . $key;
+
+				$ovMod3[$key] = $value;
+			}
+
+		} 
+foreach ($ovMod as $key => $value) {
+
+            $value = addslashes($value);
+			$value = "'$value'";
+			$updates[] = "$key=$value";
+
+		} 
+$implodeArray = implode(', ', $updates); 
+//get number of terms in update
+					//need only the keys first
+
+					$keys = implode(", ", array_keys($ovMod));
+					$keys2 = implode(", ", array_keys($ovMod3));
+			
+//get number of keys
+
+				$numberOfTerms = count($ovMod);
+		
+//echo $numberOfTerms;
+
+		$termsToInsert = ''; 
+$x=0;
+
+		foreach ($ovMod as $key=>$value){
+
+			$termsToInsert .= ( $x !== ($numberOfTerms -1) ) ? "? ," : " ?";
+
+			$x++;
+
+		} 
+$q = "UPDATE `blog_v2` SET $implodeArray WHERE `id` = '$this->id'";
+
+		
+ $stmt = $this->connection->RunQuery($q); 
+ return $stmt->rowCount(); 
+	}
+
+
+    /**
+     * Delete the row by using the key as arg
+     *
+     * @param key_table_type $key_row
+     *
+     */
+	public function Delete_row_from_key($key_row){
+		$result = $this->connection->RunQuery("DELETE FROM `blog_v2` WHERE `id` = $key_row");
+		return $result->rowCount();
+	}
+
+    /**
+     * Returns array of keys order by $column -> name of column $order -> desc or acs
+     *
+     * @param string $column
+     * @param string $order
+     */
+	public function GetKeysOrderBy($column, $order){
+		$keys = array(); $i = 0;
+		$result = $this->connection->RunQuery("SELECT id from blog_v2 order by $column $order");
+			while($row = $result->fetch_array(MYSQLI_ASSOC)){
+				$keys[$i] = $row["id"];
+				$i++;
+			}
+	return $keys;
+	}
+
+	/**
+	 * @return id - int(11)
+	 */
+	public function getid(){
+		return $this->id;
+	}
+
+	/**
+	 * @return video_id - int(11)
+	 */
+	public function getvideo_id(){
+		return $this->video_id;
+	}
+
+	/**
+	 * @return user_id - int(11)
+	 */
+	public function getuser_id(){
+		return $this->user_id;
+	}
+
+	/**
+	 * @return title - varchar(400)
+	 */
+	public function gettitle(){
+		return $this->title;
+	}
+
+	/**
+	 * @return description - varchar(1000)
+	 */
+	public function getdescription(){
+		return $this->description;
+	}
+
+	/**
+	 * @return created - timestamp
+	 */
+	public function getcreated(){
+		return $this->created;
+	}
+
+	/**
+	 * @param Type: int(11)
+	 */
+	public function setid($id){
+		$this->id = $id;
+	}
+
+	/**
+	 * @param Type: int(11)
+	 */
+	public function setvideo_id($video_id){
+		$this->video_id = $video_id;
+	}
+
+	/**
+	 * @param Type: int(11)
+	 */
+	public function setuser_id($user_id){
+		$this->user_id = $user_id;
+	}
+
+	/**
+	 * @param Type: varchar(400)
+	 */
+	public function settitle($title){
+		$this->title = $title;
+	}
+
+	/**
+	 * @param Type: varchar(1000)
+	 */
+	public function setdescription($description){
+		$this->description = $description;
+	}
+
+	/**
+	 * @param Type: timestamp
+	 */
+	public function setcreated($created){
+		$this->created = $created;
+	}
+
+    /**
+     * Close mysql connection
+     */
+	public function endblog_v2(){
+		$this->connection->CloseMysql();
+	}
+
+}
