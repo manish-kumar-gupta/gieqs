fea50f3a52919793a4f82ffc96a68343bf2a8053
David Tate <djtate@gmail.com>
2021-03-11 06:34:03 +0100

    Update check_spider_mailer.php

--- pages/learning/scripts/subscriptions/check_spider_mailer.php
+++ pages/learning/scripts/subscriptions/check_spider_mailer.php
@@ -9,9 +9,9 @@ require_once '../../../../assets/includes/config.inc.php';
 
 
 require BASE_URI . '/assets/scripts/interpretUserAccess.php';
-echo 'hello';
 
 
 error_reporting(E_ALL);
 require_once '../../../../../scripts/spider_emailer.php';
+echo 'hello';
 
