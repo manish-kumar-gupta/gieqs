c3b4f84d35307ebedd114d1545d33de61f220afd
David Tate <djtate@gmail.com>
2021-04-30 14:54:39 +0200

    Update passwordResetGenerate.php
    FIX FOR PASSWORD RESET

--- assets/scripts/passwordResetGenerate.php
+++ assets/scripts/passwordResetGenerate.php
@@ -14,6 +14,54 @@ $users = new users;
 $userFunctions = new userFunctions;
 //echo 'hello4';
 
+require(BASE_URI.'/vendor/autoload.php');    
+     
+     use PHPMailer\PHPMailer\PHPMailer;
+     use PHPMailer\PHPMailer\Exception;
+
+$debug = false;
+
+$mail = new PHPMailer;
+
+function time_elapsed_string($datetime, $full = false) {
+  $now = new DateTime;
+  $ago = new DateTime($datetime);
+  $diff = $now->diff($ago);
+
+  $diff->w = floor($diff->d / 7);
+  $diff->d -= $diff->w * 7;
+
+  $string = array(
+      'y' => 'year',
+      'm' => 'month',
+      'w' => 'week',
+      'd' => 'day',
+      'h' => 'hour',
+      'i' => 'minute',
+      's' => 'second',
+  );
+  foreach ($string as $k => &$v) {
+      if ($diff->$k) {
+          $v = $diff->$k . ' ' . $v . ($diff->$k > 1 ? 's' : '');
+      } else {
+          unset($string[$k]);
+      }
+  }
+
+  if (!$full) $string = array_slice($string, 0, 1);
+  return $string ? implode(', ', $string) . ' ago' : 'just now';
+}
+
+function get_include_contents($filename, $variablesToMakeLocal) {
+    extract($variablesToMakeLocal);
+    if (is_file($filename)) {
+        ob_start();
+        include $filename;
+        return ob_get_clean();
+    }
+    return false;
+}
+
 
 
 
@@ -85,11 +133,24 @@ if (count($data) > 0){
             $email = $users->getemail();
             $emailVaryarray['key'] = $users->getkey();
 
+            
+
             $filename = '/assets/email/emailPasswordReset.php';
 
             $subject = 'Password reset for your GIEQs online account';
 
-            require(BASE_URI . '/assets/scripts/individualMailerGmailAPI.php');  //TEST MAIL
+            $mail->CharSet = "UTF-8";
+             $mail->Encoding = "base64";
+             $mail->Subject = $subject;
+             $mail->setFrom('admin@gieqs.com', 'GIEQs Online');
+             $mail->addAddress($emailVaryarray['email']);
+             $mail->msgHTML(get_include_contents(BASE_URI . $filename, $emailVaryarray));
+             $mail->AltBody = strip_tags((get_include_contents(BASE_URI . $filename, $emailVaryarray)));
+             $mail->preSend();
+             $mime = $mail->getSentMIMEMessage();
+             $mime = rtrim(strtr(base64_encode($mime), '+/', '-_'), '=');
+
+             require(BASE_URI . '/assets/scripts/individualMailerGmailAPIPHPMailer.php');  //TEST MAIL
 
             if ($debug){
 
