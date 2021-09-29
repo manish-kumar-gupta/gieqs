a0067ab9b58ef1be5ea9f5a17c6aa15d8a187bd8
David Tate <djtate@gmail.com>
2021-09-29 11:59:31 +0200

    .

--- pages/learning/pages/live/generate_materials_live.php
+++ pages/learning/pages/live/generate_materials_live.php
@@ -192,7 +192,7 @@ $requiredUserLevel = 3;
             <div class="container-fluid d-flex flex-wrap align-items-lg-stretch p-2 p-lg-5" style="max-height:60vh;">
                 <div class="col-lg-5 ml-4" style="cursor:none;">
                     <h2 class="mb-5">LIVE area</h2>
-                                        <div class="card mb-0 p-2 tagCard mx-6" style="height:30vh; overflow:hidden; cursor:none; margin-top: 15rem !important;">
+                                        <div class="card mb-0 p-2 tagCard mx-6" style="height:30vh; overflow:hidden; cursor:none; margin-top: 10rem !important;">
                         <div class="card-header tagCardHeader mb-0">
 
                             <span class="h6">Materials <br /></span><span class="text-sm"></span><span
