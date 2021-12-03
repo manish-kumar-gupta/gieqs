8a26817a3dad6fdb70f788efef06bdb27a95e106
David Tate <djtate@gmail.com>
2021-12-03 06:25:43 +0100

    Update create-checkout-session-subscription.php

--- pages/learning/scripts/subscriptions/create-checkout-session-subscription.php
+++ pages/learning/scripts/subscriptions/create-checkout-session-subscription.php
@@ -53,7 +53,7 @@ header('Content-Type: application/json');
 error_reporting(E_ALL);
 error_reporting(E_ALL);
 
-$debug = true;
+$debug = false;
 
 $data = json_decode(file_get_contents('php://input'), true);
 //print_r($data);
