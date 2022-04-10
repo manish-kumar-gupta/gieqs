c80fc9a3fc5ad316e358ec166325bf0981529219
David Tate <djtate@gmail.com>
2022-04-10 14:21:31 +0200

    class updates

--- assets/scripts/classes/institutional.php
+++ assets/scripts/classes/institutional.php
@@ -26,18 +26,15 @@
           }
 
 
-Class token {
+Class institutional {
 
 	private $id; //int(11)
-	private $asset_id; //int(11)
-	private $cipher; //varchar(20)
+	private $name; //varchar(100)
+	private $long_name; //varchar(300)
+	private $contact; //varchar(255)
+	private $contact_email; //varchar(255)
+	private $logo_src; //varchar(400)
 	private $created; //timestamp
-	private $remaining; //varchar(20)
-	private $partner; //int(11)
-	private $sponsor; //int(11)
-	private $institutional_id; //int(11)
-	private $length; //int(11)
-	private $cost; //int(11)
 	private $connection;
 
 	public function __construct(){
@@ -50,16 +47,13 @@ Class token {
      * New object to the class. Don�t forget to save this new object "as new" by using the function $class->Save_Active_Row_as_New();
      *
      */
-	public function New_token($asset_id,$cipher,$created,$remaining,$partner,$sponsor,$institutional_id,$length,$cost){
-		$this->asset_id = $asset_id;
-		$this->cipher = $cipher;
+	public function New_institutional($name,$long_name,$contact,$contact_email,$logo_src,$created){
+		$this->name = $name;
+		$this->long_name = $long_name;
+		$this->contact = $contact;
+		$this->contact_email = $contact_email;
+		$this->logo_src = $logo_src;
 		$this->created = $created;
-		$this->remaining = $remaining;
-		$this->partner = $partner;
-		$this->sponsor = $sponsor;
-		$this->institutional_id = $institutional_id;
-		$this->length = $length;
-		$this->cost = $cost;
 	}
 
     /**
@@ -69,18 +63,15 @@ Class token {
      *
      */
 	public function Load_from_key($key_row){
-		$result = $this->connection->RunQuery("Select * from token where id = \"$key_row\" ");
+		$result = $this->connection->RunQuery("Select * from institutional where id = \"$key_row\" ");
 		while($row = $result->fetch(PDO::FETCH_ASSOC)){
 			$this->id = $row["id"];
-			$this->asset_id = $row["asset_id"];
-			$this->cipher = $row["cipher"];
+			$this->name = $row["name"];
+			$this->long_name = $row["long_name"];
+			$this->contact = $row["contact"];
+			$this->contact_email = $row["contact_email"];
+			$this->logo_src = $row["logo_src"];
 			$this->created = $row["created"];
-			$this->remaining = $row["remaining"];
-			$this->partner = $row["partner"];
-			$this->sponsor = $row["sponsor"];
-			$this->institutional_id = $row["institutional_id"];
-			$this->length = $row["length"];
-			$this->cost = $row["cost"];
 		}
 	}
     /**
@@ -90,7 +81,7 @@ Class token {
  *
  */
 	public function Load_records_limit_json($y, $x=0){
-$q = "Select * from `token` LIMIT " . $x . ", " . $y;
+$q = "Select * from `institutional` LIMIT " . $x . ", " . $y;
 		$result = $this->connection->RunQuery($q);
 							$rowReturn = array();
 						$x = 0;
@@ -99,15 +90,12 @@ $q = "Select * from `token` LIMIT " . $x . ", " . $y;
 
 					while($row = $result->fetch(PDO::FETCH_ASSOC)){
 			$rowReturn[$x]["id"] = $row["id"];
-			$rowReturn[$x]["asset_id"] = $row["asset_id"];
-			$rowReturn[$x]["cipher"] = $row["cipher"];
+			$rowReturn[$x]["name"] = $row["name"];
+			$rowReturn[$x]["long_name"] = $row["long_name"];
+			$rowReturn[$x]["contact"] = $row["contact"];
+			$rowReturn[$x]["contact_email"] = $row["contact_email"];
+			$rowReturn[$x]["logo_src"] = $row["logo_src"];
 			$rowReturn[$x]["created"] = $row["created"];
-			$rowReturn[$x]["remaining"] = $row["remaining"];
-			$rowReturn[$x]["partner"] = $row["partner"];
-			$rowReturn[$x]["sponsor"] = $row["sponsor"];
-			$rowReturn[$x]["institutional_id"] = $row["institutional_id"];
-			$rowReturn[$x]["length"] = $row["length"];
-			$rowReturn[$x]["cost"] = $row["cost"];
 		$x++;		}return json_encode($rowReturn);}
 
 			else{return FALSE;
@@ -121,7 +109,7 @@ $q = "Select * from `token` LIMIT " . $x . ", " . $y;
  *
  */
 	public function Return_row($key){
-$q = "Select * from `token` WHERE `id` = $key";
+$q = "Select * from `institutional` WHERE `id` = $key";
 		$result = $this->connection->RunQuery($q);
 							$rowReturn = array();
 						$x = 0;
@@ -130,15 +118,12 @@ $q = "Select * from `token` WHERE `id` = $key";
 
 					while($row = $result->fetch(PDO::FETCH_ASSOC)){
 			$rowReturn[$x]["id"] = $row["id"];
-			$rowReturn[$x]["asset_id"] = $row["asset_id"];
-			$rowReturn[$x]["cipher"] = $row["cipher"];
+			$rowReturn[$x]["name"] = $row["name"];
+			$rowReturn[$x]["long_name"] = $row["long_name"];
+			$rowReturn[$x]["contact"] = $row["contact"];
+			$rowReturn[$x]["contact_email"] = $row["contact_email"];
+			$rowReturn[$x]["logo_src"] = $row["logo_src"];
 			$rowReturn[$x]["created"] = $row["created"];
-			$rowReturn[$x]["remaining"] = $row["remaining"];
-			$rowReturn[$x]["partner"] = $row["partner"];
-			$rowReturn[$x]["sponsor"] = $row["sponsor"];
-			$rowReturn[$x]["institutional_id"] = $row["institutional_id"];
-			$rowReturn[$x]["length"] = $row["length"];
-			$rowReturn[$x]["cost"] = $row["cost"];
 		$x++;		}return json_encode($rowReturn);}
 
 			else{return FALSE;
@@ -149,7 +134,7 @@ $q = "Select * from `token` WHERE `id` = $key";
 
         public function Load_records_limit_json_datatables($y, $x = 0)
             {
-            $q = "Select * from `token` LIMIT $x, $y";
+            $q = "Select * from `institutional` LIMIT $x, $y";
             $result = $this->connection->RunQuery($q);
             $rowReturn = array();
             $x = 0;
@@ -181,7 +166,7 @@ $q = "Select * from `token` WHERE `id` = $key";
      *
      */
 	public function matchRecord($key_row){
-		$result = $this->connection->RunQuery("Select * from `token` where `id` = '$key_row' ");
+		$result = $this->connection->RunQuery("Select * from `institutional` where `id` = '$key_row' ");
 		$nRows = $result->rowCount();
 			if ($nRows == 1){
 				return TRUE;
@@ -194,7 +179,7 @@ $q = "Select * from `token` WHERE `id` = $key";
 		* Return the number of rows
 		*/
 	public function numberOfRows(){
-		return $this->connection->TotalOfRows('token');
+		return $this->connection->TotalOfRows('institutional');
 	}
 
     /**
@@ -272,7 +257,7 @@ $x=0;
 			$x++;
 
 		} 
-$q = "INSERT INTO `token` ($keys) VALUES ($keys2)";
+$q = "INSERT INTO `institutional` ($keys) VALUES ($keys2)";
 		
  $stmt = $this->connection->prepare($q); 
 $stmt->execute($ovMod3); 
@@ -357,7 +342,7 @@ $x=0;
 			$x++;
 
 		} 
-$q = "UPDATE `token` SET $implodeArray WHERE `id` = '$this->id'";
+$q = "UPDATE `institutional` SET $implodeArray WHERE `id` = '$this->id'";
 
 		
  $stmt = $this->connection->RunQuery($q); 
@@ -372,7 +357,7 @@ $q = "UPDATE `token` SET $implodeArray WHERE `id` = '$this->id'";
      *
      */
 	public function Delete_row_from_key($key_row){
-		$result = $this->connection->RunQuery("DELETE FROM `token` WHERE `id` = $key_row");
+		$result = $this->connection->RunQuery("DELETE FROM `institutional` WHERE `id` = $key_row");
 		return $result->rowCount();
 	}
 
@@ -384,7 +369,7 @@ $q = "UPDATE `token` SET $implodeArray WHERE `id` = '$this->id'";
      */
 	public function GetKeysOrderBy($column, $order){
 		$keys = array(); $i = 0;
-		$result = $this->connection->RunQuery("SELECT id from token order by $column $order");
+		$result = $this->connection->RunQuery("SELECT id from institutional order by $column $order");
 			while($row = $result->fetch_array(MYSQLI_ASSOC)){
 				$keys[$i] = $row["id"];
 				$i++;
@@ -400,17 +385,38 @@ $q = "UPDATE `token` SET $implodeArray WHERE `id` = '$this->id'";
 	}
 
 	/**
-	 * @return asset_id - int(11)
+	 * @return name - varchar(100)
 	 */
-	public function getasset_id(){
-		return $this->asset_id;
+	public function getname(){
+		return $this->name;
 	}
 
 	/**
-	 * @return cipher - varchar(20)
+	 * @return long_name - varchar(300)
 	 */
-	public function getcipher(){
-		return $this->cipher;
+	public function getlong_name(){
+		return $this->long_name;
+	}
+
+	/**
+	 * @return contact - varchar(255)
+	 */
+	public function getcontact(){
+		return $this->contact;
+	}
+
+	/**
+	 * @return contact_email - varchar(255)
+	 */
+	public function getcontact_email(){
+		return $this->contact_email;
+	}
+
+	/**
+	 * @return logo_src - varchar(400)
+	 */
+	public function getlogo_src(){
+		return $this->logo_src;
 	}
 
 	/**
@@ -421,48 +427,6 @@ $q = "UPDATE `token` SET $implodeArray WHERE `id` = '$this->id'";
 	}
 
 	/**
-	 * @return remaining - varchar(20)
-	 */
-	public function getremaining(){
-		return $this->remaining;
-	}
-
-	/**
-	 * @return partner - int(11)
-	 */
-	public function getpartner(){
-		return $this->partner;
-	}
-
-	/**
-	 * @return sponsor - int(11)
-	 */
-	public function getsponsor(){
-		return $this->sponsor;
-	}
-
-	/**
-	 * @return institutional_id - int(11)
-	 */
-	public function getinstitutional_id(){
-		return $this->institutional_id;
-	}
-
-	/**
-	 * @return length - int(11)
-	 */
-	public function getlength(){
-		return $this->length;
-	}
-
-	/**
-	 * @return cost - int(11)
-	 */
-	public function getcost(){
-		return $this->cost;
-	}
-
-	/**
 	 * @param Type: int(11)
 	 */
 	public function setid($id){
@@ -470,17 +434,38 @@ $q = "UPDATE `token` SET $implodeArray WHERE `id` = '$this->id'";
 	}
 
 	/**
-	 * @param Type: int(11)
+	 * @param Type: varchar(100)
 	 */
-	public function setasset_id($asset_id){
-		$this->asset_id = $asset_id;
+	public function setname($name){
+		$this->name = $name;
 	}
 
 	/**
-	 * @param Type: varchar(20)
+	 * @param Type: varchar(300)
 	 */
-	public function setcipher($cipher){
-		$this->cipher = $cipher;
+	public function setlong_name($long_name){
+		$this->long_name = $long_name;
+	}
+
+	/**
+	 * @param Type: varchar(255)
+	 */
+	public function setcontact($contact){
+		$this->contact = $contact;
+	}
+
+	/**
+	 * @param Type: varchar(255)
+	 */
+	public function setcontact_email($contact_email){
+		$this->contact_email = $contact_email;
+	}
+
+	/**
+	 * @param Type: varchar(400)
+	 */
+	public function setlogo_src($logo_src){
+		$this->logo_src = $logo_src;
 	}
 
 	/**
@@ -490,52 +475,10 @@ $q = "UPDATE `token` SET $implodeArray WHERE `id` = '$this->id'";
 		$this->created = $created;
 	}
 
-	/**
-	 * @param Type: varchar(20)
-	 */
-	public function setremaining($remaining){
-		$this->remaining = $remaining;
-	}
-
-	/**
-	 * @param Type: int(11)
-	 */
-	public function setpartner($partner){
-		$this->partner = $partner;
-	}
-
-	/**
-	 * @param Type: int(11)
-	 */
-	public function setsponsor($sponsor){
-		$this->sponsor = $sponsor;
-	}
-
-	/**
-	 * @param Type: int(11)
-	 */
-	public function setinstitutional_id($institutional_id){
-		$this->institutional_id = $institutional_id;
-	}
-
-	/**
-	 * @param Type: int(11)
-	 */
-	public function setlength($length){
-		$this->length = $length;
-	}
-
-	/**
-	 * @param Type: int(11)
-	 */
-	public function setcost($cost){
-		$this->cost = $cost;
-	}
-
     /**
      * Close mysql connection
      */
-	public function endtoken(){
+	public function endinstitutional(){
 		$this->connection->CloseMysql();
 	}
 
