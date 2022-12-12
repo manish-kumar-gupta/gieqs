2445fdb14e51398be960533b722a3e2297f7bd34
David Tate <djtate@gmail.com>
2022-12-12 06:03:01 +0100

    Update update_sql_for_wordpress.php

--- pages/backend/update_sql_for_wordpress.php
+++ pages/backend/update_sql_for_wordpress.php
@@ -29,11 +29,11 @@ echo "connected";
 }
 
   // Select database
-  mysqli_select_db($database);
+  mysqli_select_db($con, $database);
 
   // List all tables in database
   $sql = "SHOW TABLES FROM ".$database;
-  $tables_result = mysql_query($sql);
+  $tables_result = mysqli_query($con, $sql);
 
   if (!$tables_result) {
     echo "Database error, could not list tables\nMySQL error: " . mysql_error();
@@ -43,7 +43,7 @@ echo "connected";
   echo "In these fields '$string_to_replace' have been replaced with '$new_string'\n\n";
   while ($table = mysqli_fetch_row($tables_result)) {
     echo "Table: {$table[0]}\n";
-    $fields_result = mysqli_query("SHOW COLUMNS FROM ".$table[0]);
+    $fields_result = mysqli_query($con, "SHOW COLUMNS FROM ".$table[0]);
     if (!$fields_result) {
       echo 'Could not run query: ' . mysqli_error();
       exit;
@@ -53,12 +53,12 @@ echo "connected";
         if (stripos($field['Type'], "VARCHAR") !== false || stripos($field['Type'], "TEXT") !== false) {
           echo "  ".$field['Field']."\n";
           $sql = "UPDATE ".$table[0]." SET ".$field['Field']." = replace(".$field['Field'].", '$string_to_replace', '$new_string')";
-          mysql_query($sql);
+          mysql_query($con, $sql);
         }
       }
       echo "\n";
     }
   }
 
-  mysqli_free_result($tables_result);  
+  mysqli_free_result($con, $tables_result);  
 ?>
