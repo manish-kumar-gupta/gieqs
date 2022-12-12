089b2c006ecdd573bd41f7f0d43bfb03d1c182a9
David Tate <djtate@gmail.com>
2022-12-12 05:45:15 +0100

    Create update_sql_for_wordpress.php

---
+++ pages/backend/update_sql_for_wordpress.php
@@ -0,0 +1,47 @@
+<?php  
+  header("Content-Type: text/plain");
+
+  $host = "localhost";
+  $username = "djt35";
+  $password = "nevira1pine";
+  $database = "wordpress";
+  $string_to_replace  = 'localhost:90/dashboard/gieqs/assets/wp';
+  $new_string = 'https://www.gieqs.com/assets/wp';
+
+  // Connect to database server
+  mysql_connect($host, $username, $password);
+
+  // Select database
+  mysql_select_db($database);
+
+  // List all tables in database
+  $sql = "SHOW TABLES FROM ".$database;
+  $tables_result = mysql_query($sql);
+
+  if (!$tables_result) {
+    echo "Database error, could not list tables\nMySQL error: " . mysql_error();
+    exit;
+  }
+
+  echo "In these fields '$string_to_replace' have been replaced with '$new_string'\n\n";
+  while ($table = mysql_fetch_row($tables_result)) {
+    echo "Table: {$table[0]}\n";
+    $fields_result = mysql_query("SHOW COLUMNS FROM ".$table[0]);
+    if (!$fields_result) {
+      echo 'Could not run query: ' . mysql_error();
+      exit;
+    }
+    if (mysql_num_rows($fields_result) > 0) {
+      while ($field = mysql_fetch_assoc($fields_result)) {
+        if (stripos($field['Type'], "VARCHAR") !== false || stripos($field['Type'], "TEXT") !== false) {
+          echo "  ".$field['Field']."\n";
+          $sql = "UPDATE ".$table[0]." SET ".$field['Field']." = replace(".$field['Field'].", '$string_to_replace', '$new_string')";
+          mysql_query($sql);
+        }
+      }
+      echo "\n";
+    }
+  }
+
+  mysql_free_result($tables_result);  
+?>
