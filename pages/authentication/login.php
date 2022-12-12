7bb4be2823bca98ac25074f5fe9902ce00d4e680
David Tate <djtate@gmail.com>
2022-12-12 10:27:53 +0100

    Update login.php

--- pages/authentication/login.php
+++ pages/authentication/login.php
@@ -1,6 +1,7 @@
+<?php
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
-<?php require '../../assets/includes/config.inc.php';?>
+require '../../assets/includes/config.inc.php';?>
 
 <head>
 <?php
