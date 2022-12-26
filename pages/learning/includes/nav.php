99d2720307fa57c118edf6789eca2ddc234a1e35
David Tate <djtate@gmail.com>
2022-12-26 06:21:21 +0000

    .

--- pages/learning/includes/nav.php
+++ pages/learning/includes/nav.php
@@ -5,6 +5,11 @@ $debug = false;
 $_SESSION['debug'] = false;
 //error_reporting(E_ALL);
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
 function endsWith($haystack, $needle)
 {
     $length = strlen($needle);
