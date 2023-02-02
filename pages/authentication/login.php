5d058c4f571385db4f942fe5fee074c9acef9f6a
David Tate <djtate@gmail.com>
2023-02-02 16:57:47 +0100

    Update login.php

--- pages/authentication/login.php
+++ pages/authentication/login.php
@@ -1,13 +1,25 @@
 <?php
 
 require '../../assets/includes/config.inc.php';
+require (BASE_URI . '/assets/scripts/login_functions.php');
+     
+     //place to redirect the user if not allowed access
+     $location = BASE_URL . '/pages/authentication/login.php';
+ 
+     if (!($dbc)){
+     require(DB);
+     }
+    
+     
+     require(BASE_URI . '/assets/scripts/interpretUserAccess.php');
+
+     $registrationURL = BASE_URL . '/pages/program/registration.php';
 
 //define user access level
 
 $openaccess = 1;
 
 
-require BASE_URI . '/head.php';
 
 
 require_once BASE_URI . '/assets/scripts/classes/assetManager.class.php';
@@ -284,6 +296,36 @@ redirect_user(BASE_URL . '/' . $target);
 ?>
     <title>Ghent International Endoscopy Symposium - Login </title>
   
+
+<!--iphone fix -->
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+ <!-- Favicon -->
+ <link rel="icon" href="<?php echo BASE_URL;?>/assets/img/brand/favicongieqs.png" type="image/png">
+    <!-- Font Awesome 5 -->
+    <link rel="stylesheet" href="<?php echo BASE_URL;?>/assets/libs/@fortawesome/fontawesome-free/css/all.min.css"><!-- Page CSS -->
+    <link rel="stylesheet" href="<?php echo BASE_URL;?>/assets/libs/swiper/dist/css/swiper.min.css">
+    <link rel="stylesheet" href="<?php echo BASE_URL;?>/assets/libs/@fancyapps/fancybox/dist/jquery.fancybox.min.css">
+    <!--Extra CSS -->
+    <link rel="stylesheet" href="<?php echo BASE_URL;?>/assets/libs/select2/dist/css/select2.min.css">
+
+    <!-- <link rel="stylesheet" href="<?php echo BASE_URL;?>/assets/libs/select2/dist/css/select2bootstrap.min.css">
+     -->
+    <!-- Purpose CSS -->
+    <link rel="stylesheet" href="<?php echo BASE_URL;?>/assets/css/purpose.css" id="stylesheet">
+   
+    <link rel="stylesheet" href="<?php echo BASE_URL;?>/assets/css/gieqs.css">
+
+<!-- Global JS -->
+
+<script src="<?php echo BASE_URL;?>/assets/js/purpose.core.js"></script>
+<script src="<?php echo BASE_URL;?>/assets/libs/select2/dist/js/select2.min.js"></script>
+<!-- <script src="<?php //echo BASE_URL;?>/assets/js/purpose.js"></script>
+ --><script src="<?php echo BASE_URL;?>/assets/js/generaljs.js"></script>
+<script src="<?php echo BASE_URL;?>/node_modules/jquery-validation/dist/jquery.validate.js"></script>
+
+
     <style>
 
       /* Change the white to any color ;) */
